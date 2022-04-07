0878dd802f1f5758c9f53ab93c8ec9f349e98a3c
Alex Mwangi <alex@stl-horizon.com>
2022-04-07 16:22:02 +0300

    Added routes for Crud

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BrandController;
 use App\Http\Controllers\CategoryController;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
@@ -22,17 +23,16 @@ Route::get('/', function () {
 
 // Category
 Route::get('category/all', [CategoryController::class, 'index'])->name('all.category');
-
 Route::post('category/add', [CategoryController::class, 'store'])->name('store.category');
-
 Route::get('category/edit/{id}', [CategoryController::class, 'edit']);
-
 Route::post('category/update/{id}', [CategoryController::class, 'update']);
-
 Route::get('category/softdelete/{id}', [CategoryController::class, 'SoftDelete']);
 Route::get('category/restore/{id}', [CategoryController::class, 'Restore']);
 Route::get('category/delete_permanently/{id}', [CategoryController::class, 'DeletePermanent']);
 
+// BRAND
+Route::get('brand/all', [BrandController::class, 'index'])->name('all.brand');
+Route::post('brand/add', [BrandController::class, 'store'])->name('store.brand');
 
 
 Route::middleware(['auth:sanctum',  config('jetstream.auth_session'), 'verified'])->group(function () {
