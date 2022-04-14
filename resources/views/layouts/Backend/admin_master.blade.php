acdf7e5d8e99ab4857df70cc21ec22ad6a989f37
Alex Mwangi <alex@stl-horizon.com>
2022-04-14 12:22:25 +0300

    Added script section for toastr message

--- resources/views/layouts/Backend/admin_master.blade.php
+++ resources/views/layouts/Backend/admin_master.blade.php
@@ -92,8 +92,35 @@
 {{-- Toastr --}}
 <script src="{{ asset('backend/assets/js/toastr.min.js') }}"></script>
 
-     @yield('scripts')
+     {{-- @yield('scripts') --}}
 
+     <script>
+
+       @if(Session::has('message'))
+
+       let type = "{{ Session::get('alert-type' , 'info')}}"
+
+       switch (type) {
+
+         case 'info':
+           toastr.info("{{ Session::get('message')}}");
+           break;
+
+         case 'success':
+           toastr.success("{{ Session::get('message')}}");
+           break;
+
+         case 'warning':
+           toastr.warning("{{ Session::get('message')}}");
+           break;
+
+         case 'error':
+           toastr.error("{{ Session::get('message')}}");
+           break;              
+       }
+
+       @endif
+     </script>
      
   </body>
 </html>
